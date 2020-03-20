014429f4bd552c691015ba1adc18a2f913e44946
sbeard <stevenbeard@softwarepricing.com>
2020-03-20 00:33:46 -0400

    code revisions, added sam deployment data

--- src/App.js
+++ src/App.js
@@ -1,119 +1,186 @@
 import React, { useState, useEffect } from "react";
-import Scrollbar from "react-scrollbars-custom";
+import axios from "axios";
+// import Scrollbar from "react-scrollbars-custom";
 import "./App.css";
 
 function App() {
-  const [worldSummary, setWorldSummary] = useState(null);
-  const [usDeaths, setusDeaths] = useState(null);
-  const [usConfirmed, setusConfirmed] = useState(null);
-  const [usRecovered, setusRecovered] = useState(null);
+  const [data, setData] = useState();
 
+  // Call apis to retrieve data for the page ** hard coded for single use on render
+  const fetchData = async () => {
+    const [
+      resSummary,
+      resUsDeaths,
+      resUsConfirmed,
+      resUsRecovered
+    ] = await Promise.all([
+      axios("https://api.covid19api.com/summary"),
+      axios("https://api.covid19api.com/country/US/status/deaths"),
+      axios("https://api.covid19api.com/country/US/status/confirmed"),
+      axios("https://api.covid19api.com/country/US/status/recovered")
+    ]);
+    setData({
+      summary: resSummary,
+      usDeaths: resUsDeaths,
+      usConfirmed: resUsConfirmed,
+      usRecovered: resUsRecovered
+    });
+  };
+
+  /**
+   * Return combined total of key for all objects
+   * @param {Object[]} array
+   * @param {string} key - obj key to add
+   */
+  const valueAdd = (array, key) => {
+    array
+      .reduce((a, b) => {
+        return a + b[key];
+      }, 0)
+      .toLocaleString();
+  };
+
+  /**
+   * Use only the items with the most recent date and prepare items to be merged (Cases:n needs to have a unique key)
+   * @param {Object[]} array
+   * @param {string} newKey - name of key to change to
+   * @param {string} oldKey - name of key to change from
+   * @param {Date} lastUpdateTime - Date to filter objects
+   */
+  const arrayScrubber = (array, lastUpdateTime, newKey, oldKey) => {
+    return array
+      .filter(item => new Date(item.Date) === lastUpdateTime)
+      .map(item => ({
+        ...item,
+        [newKey]: item[oldKey]
+      }));
+  };
+
+  /**
+   * Merge (not concat) objects from each array
+   * @param {Array.Array.<Objects>} arrays
+   * @param {string} key - key to merge on
+   */
+  const mergeArrays = (arrays, key) => {
+    const merged = {};
+    arrays.forEach(arr => {
+      arr.forEach(item => {
+        merged[item[key]] = Object.assign({}, merged[item[key]], item);
+      });
+    });
+    return Object.values(merged);
+  };
+
+  /**
+   * Return most recent date
+   * @param {Object[]} array
+   * @param {'string'} key for date value in objects
+   */
+  const lastUpdated = (array, key) => {
+    new Date(
+      Math.max.apply(
+        null,
+        array.map(function(item) {
+          return new Date(item["key"]);
+        })
+      )
+    );
+  };
+
+  // Handle state
   useEffect(() => {
     try {
-      fetch("https://api.covid19api.com/summary")
-        .then(res => res.json())
-        .then(setWorldSummary);
-
-      fetch("https://api.covid19api.com/country/US/status/deaths")
-        .then(res => res.json())
-        .then(setusDeaths);
-
-      fetch("https://api.covid19api.com/country/US/status/confirmed")
-        .then(res => res.json())
-        .then(setusConfirmed);
-
-      fetch("https://api.covid19api.com/country/US/status/recovered")
-        .then(res => res.json())
-        .then(setusRecovered);
+      fetchData();
     } catch (err) {
+      //TODO add error handling
       console.log(err);
     }
   }, []);
 
-  let updateDate = null;
-  let totalDeath = null;
-  let totalConfirmed = null;
-  let totalRecovered = null;
-  let usTotal = null;
-  if (worldSummary) {
-    worldSummary.Countries.sort((a, b) =>
-      a.TotalConfirmed > b.TotalConfirmed ? -1 : 1
-    );
-    updateDate = new Date(worldSummary.Date).toString().split("G")[0];
-    totalDeath = worldSummary.Countries.reduce((a, b) => {
-      return a + b.TotalDeaths;
-    }, 0).toLocaleString();
-    totalConfirmed = worldSummary.Countries.reduce((a, b) => {
-      return a + b.TotalConfirmed;
-    }, 0).toLocaleString();
-    totalRecovered = worldSummary.Countries.reduce((a, b) => {
-      return a + b.TotalRecovered;
-    }, 0).toLocaleString();
-    usTotal = worldSummary.Countries.filter(
-      Countries => Countries.Country === "US"
-    );
-  }
+  // add new state variables
+  useEffect(() => {
+    if (data.summary && data.summary !== "") {
+      // all list sorted by Cases confirmed
+      data.summary.Countries.sort((a, b) =>
+        a.TotalConfirmed > b.TotalConfirmed ? -1 : 1
+      );
 
-  let usComplete;
+        // Get Totals for all countries
+        const totalDeath = valueAdd(data.summary.Countries, "TotalDeaths");
+        const totalConfirmed = valueAdd(data.summary.Countries, "TotalConfirmed");
+        const totalRecovered = valueAdd(data.summary.Countries, "TotalRecovered");
+  
+        const usTotal = data.summary.Countries.filter(
+          Countries => Countries.Country === "US"
+        );
 
-  if (usRecovered !== null && usDeaths !== null && usConfirmed !== null) {
-    const arrayScrubber = (arr, newKey) => {
-      return arr
-        .filter(item => item.Date === "2020-03-18T00:00:00Z")
-        .map(item => ({
-          Country: item.Country,
-          Province: item.Province,
-          Lat: item.Lat,
-          Lon: item.Lon,
-          Date: item.Date,
-          [newKey]: item.Cases
-        }))
-        .sort((a, b) => {
-          a = new Date(a.Date);
-          b = new Date(b.Date);
-          return a > b ? -1 : a < b ? 1 : 0;
-        });
-    };
-    let deathScrubbed = arrayScrubber(usDeaths, "death");
-    let confirmedScrubbed = arrayScrubber(usConfirmed, "confirmed");
-    let recoveredScrubbed = arrayScrubber(usRecovered, "recovered");
+      // Get last date usDeaths list / summary was updated
+      const lastUsUpdate = lastUpdated(usDeaths, "Date");
+      const updateDate = new Date(data.summary.Date).toString().split("G")[0];
 
-    const mergeArrays = (arrays, prop) => {
-      const merged = {};
+      // scrub arrays for most recent time and change object key to prepare for merge
+      let deathScrubbed = arrayScrubber(
+        usDeaths,
+        "death",
+        "Cases",
+        lastUsUpdate
+      );
+      let confirmedScrubbed = arrayScrubber(
+        usConfirmed,
+        "confirmed",
+        "Cases",
+        lastUsUpdate
+      );
+      let recoveredScrubbed = arrayScrubber(
+        usRecovered,
+        "recovered",
+        "Cases",
+        lastUsUpdate
+      );
 
-      arrays.forEach(arr => {
-        arr.forEach(item => {
-          merged[item[prop]] = Object.assign({}, merged[item[prop]], item);
-        });
+      // Merge arrays to combine aggregate data for death, confirmed, recovered 
+      let holder = mergeArrays([deathScrubbed, confirmedScrubbed], "Province");
+      let usComplete;
+      if (holder) usComplete = mergeArrays([holder, recoveredScrubbed], "Province");
+
+      // sort array for use by confirmation
+      if (usComplete) usComplete.sort((a, b) => b.confirmed - a.confirmed);
+
+      setData({
+        ...data,
+        lastUpdated: updateDate,
+        worldTotatDeaths: totalDeath,
+        worldTotalConfirmed: totalConfirmed,
+        worldTotalRecovered: totalRecovered,
+        usTotals: usTotal,
+        usCompleted: usComplete
       });
+    }
+  }, []);
 
-      return Object.values(merged);
-    };
-    let holder = mergeArrays([deathScrubbed, confirmedScrubbed], "Province");
-
-    if (holder)
-      usComplete = mergeArrays([holder, recoveredScrubbed], "Province");
-  }
-
-  if (usComplete) usComplete.sort((a, b) => b.confirmed - a.confirmed);
   return (
     <main className="App">
       <header className={"box"}>
         <h2>Track the Coronavirus</h2>
-        <p>Data is sourced from <a href={'https://github.com/CSSEGISandData/COVID-19'}>Johns Hopkins CSSE</a></p>
+        <p>
+          Data is sourced from{" "}
+          <a href={"https://github.com/CSSEGISandData/COVID-19"}>
+            Johns Hopkins CSSE
+          </a>
+        </p>
         <span>(Updated: {updateDate})</span>
       </header>
 
-      <article id="worldSummary" className={"box"}>
+      <article id="data.summary" className={"box"}>
         <span>
           <h3>
             WORLD SUMMARY<hr></hr>
           </h3>
         </span>
         <Scrollbar style={{ width: 370, height: 600 }}>
-          <section id={"worldSummaryData"}>
-            {worldSummary
-              ? worldSummary.Countries.map((item, index) => (
+          <section id={"data.summaryData"}>
+            {data.summary
+              ? data.summary.Countries.map((item, index) => (
                   <section key={index + 400}>
                     <span key={index + 200} style={{ color: "red" }}>
                       {item.TotalDeaths.toLocaleString()}
@@ -205,12 +272,12 @@ function App() {
                       {item.death}
                     </span>
                     <span key={index + 300} style={{ color: "yellow" }}>
-                      {item.confirmed}
+                      {item.confirmed.toLocaleString()}
                     </span>
                     <span key={index + 400} style={{ color: "green" }}>
-                      {item.recovered}
+                      {item.recovered.toLocaleString()}
                     </span>
-                    {item.Province}
+                    {item.Province.toLocaleString()}
                     <hr />
                   </section>
                 ))
